94fa5e220c239d4372665e4be170543dafd10efc
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-24 14:10:15 +0300

    Создание новых api в OrderController
    Update, Info

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\OrderStatusRequest;
+use App\Http\Resources\OrderResource;
+use App\Models\Order;
+use App\Services\ResponseService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
 
 class OrderController extends Controller
 {
-    //
+    /**
+     * Принимает id заказа в route
+     * Меняет статус заказа
+     * @param OrderStatusRequest $request
+     * @param Order $order
+     * @return Response|Application|ResponseFactory
+     */
+    public function update(OrderStatusRequest $request, Order $order): Response|Application|ResponseFactory
+    {
+        $order->update($request->validated());
+        return ResponseService::success(
+            $order,
+            201
+        );
+    }
+
+    /**
+     * @param Order $order
+     * @return Application|Response|ResponseFactory
+     */
+    public function info(Order $order): Application|ResponseFactory|Response
+    {
+        return response(OrderResource::make($order));
+    }
 }
