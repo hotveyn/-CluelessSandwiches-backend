d9e9d74b920684d46376a2782917d82005d28b70
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-24 14:09:46 +0300

    Создание новых api в ProductController
    Info, categoryInfo, optionUpdate

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProtuctOptionUpdateRequest;
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProductResource;
+use App\Models\Option;
+use App\Models\Product;
+use App\Services\ResponseService;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Принимает id продукта
+     * Возвращает категорию с которой связан продукт
+     * @param Product $product
+     * @return Response|Application|ResponseFactory
+     */
+    public function categoryInfo(Product $product): Response|Application|ResponseFactory
+    {
+        return ResponseService::success(CategoryResource::collection($product->category()->get()));
+    }
+
+    /**
+     * Принимает id продокута
+     * Возвращает полною информацию о продукте
+     * @param Product $product
+     * @return Response|Application|ResponseFactory
+     */
+    public function info(Product $product): Response|Application|ResponseFactory
+    {
+        return ResponseService::success(ProductResource::make($product));
+    }
+
+    public function optionUpdate(ProtuctOptionUpdateRequest $request, Product $product, Option $option)
+    {
+        $option->update($request->validated());
+        return response($option);
+    }
 }
