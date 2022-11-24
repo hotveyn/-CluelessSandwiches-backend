9ab682b7386d0c55012b46c101a8c962acee8998
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-24 14:07:50 +0300

    Изменил сид

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,13 +22,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         Category::factory(20)->create()->each(function (Category $category) {
-            Product::factory(1)->create([
+            Product::factory(2)->create([
                 "category_id" => $category->id
             ])->each(function (Product $product) use ($category) {
-                Option::factory(1)->create()->each(function (Option $option) use ($category, $product) {
-                    ProductOption::factory(5)->create([
-                        "product_id" => $product->id,
-                        "option_id" => $option->id,
+                ProductOption::factory(1)->create([
+                    "product_id" => $product->id
+                ])->each(function (ProductOption $productOption) use ($category, $product) {
+                    Option::factory(3)->create([
+                        "product_option_id" => $productOption->id
                     ]);
                     Order::factory(5)->create()->each(function (Order $order) use ($product) {
                         OrderProduct::factory(2)->create([
