9708d226ae934391de896ebf9accae0d0d48157b
Vanya <61887221+hotveyn@users.noreply.github.com>
2022-11-24 22:30:08 +0300

    Добавление новых реквестов и удаление ненужных

--- app/Http/Requests/OptionUpdateRequest.php
+++ app/Http/Requests/OptionUpdateRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class ProtuctOptionUpdateRequest extends FormRequest
+class OptionUpdateRequest extends FormRequest
 {
     public function rules()
     {
---
+++ app/Http/Requests/OrderStoreRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class OrderStoreRequest extends FormRequest
+{
+    public function rules()
+    {
+        return [
+            "products"=>[
+                [
+                    "product_id" => "required|integer|min:1",
+                    "product_count" => "required|integer|min:1"
+                ]
+            ]
+        ];
+    }
+}
