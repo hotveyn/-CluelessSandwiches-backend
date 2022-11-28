a9a84b5c30c9364e9f50e4b7cd4b3ee2836133f1
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:33:34 +0300

    Update OptionResource.php

--- app/Http/Resources/OptionResource.php
+++ app/Http/Resources/OptionResource.php
@@ -15,6 +15,7 @@ class OptionResource extends JsonResource
     public function toArray($request)
     {
         return [
+            "id"=>$this->id,
             "name" => $this->name,
             "toggle" => $this->toggle
         ];
