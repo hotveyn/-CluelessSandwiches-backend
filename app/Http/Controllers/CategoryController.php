8746325106fb8a8d4e800934db1664d5e8089525
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-28 15:33:39 +0300

    Update CategoryController.php

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\ProductResource;
+use App\Models\Category;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class CategoryController extends Controller
 {
-    //
+    /**
+     * @return Response|Application|ResponseFactory
+     */
+    function info(): Response|Application|ResponseFactory
+    {
+        return response(Category::all());
+    }
+    function infoOne(Category $category): Response|Application|ResponseFactory
+    {
+        return response(ProductResource::collection($category->products));
+    }
+
 }
