823a71d43679c5e0997103170470baea20e5385d
dyrteg <61887221+hotveyn@users.noreply.github.com>
2022-11-30 14:16:42 +0300

    Update OrderStoreRequest.php

--- app/Http/Requests/OrderStoreRequest.php
+++ app/Http/Requests/OrderStoreRequest.php
@@ -9,10 +9,16 @@ class OrderStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            "products"=>[
+            "products" => [
                 [
                     "product_id" => "required|integer|min:1",
-                    "product_count" => "required|integer|min:1"
+                    "product_count" => "required|integer|min:1",
+                    "options" => [
+                        [
+                            "name" => "required|string",
+                            "toggle" => "required|boolean"
+                        ]
+                    ]
                 ]
             ]
         ];
